876eaedee5c450bff653007202f2e3ead8df0715
Ilham Yusuf <ilham.0992@students.amikom.ac.id>
2022-08-01 06:09:42 +0700

    [ADD] Landing Page

---
+++ pages/popular.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { BiArrowBack } from "react-icons/bi";
+
+function popular() {
+	return (
+		<div>
+			<div>
+				<div>
+					<BiArrowBack />
+				</div>
+				<h3>Popular Menu</h3>
+			</div>
+		</div>
+	);
+}
+
+export default popular;
