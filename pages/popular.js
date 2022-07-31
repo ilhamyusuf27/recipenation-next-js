607c93ebd72cec6e96edf8c2a145f1bf70ba79e2
Ilham Yusuf <ilham.0992@students.amikom.ac.id>
2022-08-01 06:47:00 +0700

    [ADD] popular page

--- pages/popular.js
+++ pages/popular.js
@@ -1,15 +1,323 @@
 import React from "react";
 
-import { BiArrowBack } from "react-icons/bi";
+import popularStyle from "../styles/pages/popular.module.scss";
+
+import { IoIosArrowBack } from "react-icons/io";
+import { IoBookmarkOutline } from "react-icons/io5";
+import { BiLike } from "react-icons/bi";
 
 function popular() {
 	return (
 		<div>
-			<div>
-				<div>
-					<BiArrowBack />
+			{/* Title Menu */}
+			<div className={popularStyle.titlePage}>
+				<div className={popularStyle.back}>
+					<IoIosArrowBack size={25} />
 				</div>
 				<h3>Popular Menu</h3>
+			</div>
+
+			{/* Popular Menu */}
+			<div className={popularStyle.contentContainer}>
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="mb-3">
+					<div className="row">
+						<div className="col-3">
+							<div className={popularStyle.imageContent}></div>
+						</div>
+						<div className="col-5">
+							<div className={popularStyle.contentText}>
+								<h4>Margherita</h4>
+								<p>In Veg Pizza</p>
+								<h5>Spicy</h5>
+							</div>
+						</div>
+						<div className="col-4">
+							<div className={popularStyle.buttonControl}>
+								<button type="button" className="btn btn-primary">
+									<IoBookmarkOutline size={20} />
+								</button>
+								<button type="button" className="btn btn-primary">
+									<BiLike size={20} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
